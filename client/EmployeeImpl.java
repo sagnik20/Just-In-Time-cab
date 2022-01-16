6b8f2ec418d222111fc32e44f494502f9d45c98b
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2022-01-16 22:30:58 +0530

    Add files via upload

---
+++ client/EmployeeImpl.java
@@ -0,0 +1,17 @@
+package com.justintime.client;
+
+import com.justintime.modal.Employee;
+
+public class EmployeeImpl extends Employee{
+
+	public EmployeeImpl(Integer id, String name, String email, String dept) {
+		super(id, name, email, dept);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ client/ManagerImpl.java
@@ -0,0 +1,17 @@
+package com.justintime.client;
+
+import com.justintime.modal.Employee;
+
+public class ManagerImpl extends Employee{
+	
+	public ManagerImpl(Integer id, String name, String email, Boolean manager, String dept) {
+		super(id, name, email, manager, dept);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
