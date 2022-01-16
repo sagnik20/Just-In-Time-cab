bc9a944ca7307f6bf5204ef01b3f35aa39b0eeb9
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2022-01-16 22:32:53 +0530

    Add files via upload

--- src/com/justintime/client/ManagerImpl.java
+++ src/com/justintime/client/ManagerImpl.java
@@ -9,9 +9,4 @@ public class ManagerImpl extends Employee{
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
